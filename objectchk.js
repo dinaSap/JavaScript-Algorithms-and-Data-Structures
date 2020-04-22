fdca036ca4396008c27479eeeb22493ef591930c
dinaSap <49748840+dinaSap@users.noreply.github.com>
2020-04-22 23:40:32 +0400

    Update objectchk.js

--- objectchk.js
+++ objectchk.js
@@ -3,7 +3,6 @@ function whatIsInAName(collection, source) {
   // Only change code below this line
  let arr1 = Object.keys(source)
 for (let i = 0; i < collection.length; i++){
-  let arr2 = Object.keys(collection[i])
   if (arr1.every(el => source[el] === collection[i][el])){
     arr.push(collection[i])
   }
