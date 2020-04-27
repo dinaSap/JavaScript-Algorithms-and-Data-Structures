fa793e64ab0102b6f9c8ee06a55ee0a32f9152ef
dinaSap <49748840+dinaSap@users.noreply.github.com>
2020-04-28 01:54:32 +0400

    Update sumPrimes.js

--- sumPrimes.js
+++ sumPrimes.js
@@ -1,14 +1,17 @@
 function sumPrimes(num) {
   let arr = []
-  for (let i = 2; i <= num; i++) {
-    arr.push(i);
-}
-const isPrime = num => {
+  
+  const isPrime = num => {
   for(let i = 2; i < num; i++)
     if(num % i === 0) {return false};
   return true;
 }
-  arr = arr.filter(el => isPrime(el)).reduce((a, b) => a+b)
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)){
+    arr.push(i);}
+}
+
+  arr = arr.reduce((a, b) => a+b)
   
 return arr;
 }
